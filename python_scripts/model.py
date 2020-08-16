63f8a74d724c87a18409d123b6d1723d6bac6dd9
yashjjw <yashjhunjhunwala8@gmail.com>
2020-08-16 15:59:23 +0530

    ml and dataset improved

--- python_scripts/model.py
+++ python_scripts/model.py
@@ -3,33 +3,31 @@ import pandas as pd
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import confusion_matrix
 
 if __name__ == "__main__":
     # Importing the dataset
-    df = pd.read_csv('dummy_leads_data.csv')
+    df = pd.read_csv('out.csv')
     # Shuffling the data
     df = df.sample(frac=1).reset_index(drop=True)
     
-    X = df.iloc[:, 0:9].values
-    y = df.iloc[:, 9].values
+    X = df.iloc[:, 0:5].values
+    y = df.iloc[:, 5].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size = 0.25, random_state = 42)
 
-    # sc = StandardScaler()
-    # X_train = sc.fit_transform(X_train)
-    # X_test = sc.transform(X_test)
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
-    classifier = RandomForestClassifier(n_estimators = 300, criterion = 'entropy', random_state = 42)
+    classifier = RandomForestRegressor(n_estimators = 300 , random_state = 42)
     classifier.fit(X_train, y_train)
 
-    y_pred = classifier.predict(X_test)
-
-    cm = confusion_matrix(y_test, y_pred)
+  
 
     pickle.dump(classifier , open('..//data//model.pkl', 'wb'))
 
     loaded_model = pickle.load(open('..//data//model.pkl', 'rb'))
-    result = loaded_model.score(X_test, y_test)
-    print(result)
-    print(cm)
+
+    y_pred = loaded_model.predict(X_test)
+  
